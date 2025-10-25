8d3ae7090db58f79d3ab25b9c441786411633a47
Mahimaa12 <mahimaa.umashankar@gmail.com>
2025-10-25 23:19:05 +0530

    Finalizing DB connection parameters and fixing PHP tag.

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,13 +1,16 @@
 <?php
-  session_start();
-  $servername = "hms.test"; //change this  accordingly
-  $dBUsername = "root";
-  $dBPassword = "root";
-  $dBName = "hostel_management_system";
- // session_start();
-  $conn=mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+session_start();
+ 
+$servername = "localhost"; // Changed from hms.test to localhost
+$dBUsername = "root";
+$dBPassword = ""; // Changed from 'root' to blank for XAMPP default
+$dBName = "hostel_db"; // Changed from 'hostel_management_system'
+$dBPort = 3307; // Added the custom MySQL port
 
-  if (!$conn) {
-    die("Connection Failed: ".mysqli_connect_error());
-  }
+// Now use the variables above in the connect function:
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName, $dBPort);
+
+if (!$conn) {
+  die("Connection Failed: ".mysqli_connect_error());
+}
 ?>
